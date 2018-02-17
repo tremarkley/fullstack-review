d8d37af69a21f78698445d41b59b32ba5e57bca1
Harry Markley <tremarkley@gmail.com>
2018-02-17 11:15:24 -0800

    Refactor constructor for document save to use es6 object shorthand notation

--- database/index.js
+++ database/index.js
@@ -16,9 +16,14 @@ let Repo = mongoose.model('Repo', repoSchema);
 let save = (arrayRepos) => {
   for (let i = 0; i < arrayRepos.length; i++) {
     //rename fullRepo to be more transparent that it'll be a document and use es6 Object Shorthand Notation to clean up line 21
+    
     var fullRepo = arrayRepos[i];
-    var owner = fullRepo.owner.login;
-    var newRepo = new Repo({id: fullRepo.id, name: fullRepo.name, owner: owner, url: fullRepo.html_url, description: fullRepo.description, stars: fullRepo.stargazers_count});
+    fullRepo.owner = fullRepo.owner.login;
+    fullRepo.url = fullRepo.html_url;
+    fullRepo.stars = fullRepo.stargazers_count;
+    //var owner = fullRepo.owner.login;
+    //var newRepo = new Repo({id: fullRepo.id, name: fullRepo.name, owner: owner, url: fullRepo.html_url, description: fullRepo.description, stars: fullRepo.stargazers_count});
+    var newRepo = new Repo(fullRepo);
     newRepo.save();
   }
 }
