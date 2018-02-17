f70326057b8ac93ab9e53cc214fb03e5b597d677
Harry Markley <tremarkley@gmail.com>
2018-02-16 19:25:22 -0800

    Added reference to env variable instead of config token

--- helpers/github.js
+++ helpers/github.js
@@ -1,5 +1,5 @@
 const request = require('request');
-const config = require('../config.js');
+//const config = require('../config.js');
 
 let getReposByUsername = (username, callback) => {
   // TODO - Use the request module to request repos for a specific
@@ -11,7 +11,7 @@ let getReposByUsername = (username, callback) => {
     url: `https://api.github.com/users/${username}/repos`,
     headers: {
       'User-Agent': 'request',
-      'Authorization': `token ${config.TOKEN}`
+      'Authorization': `token ${process.env.GITHUB_API_TOKEN}`
     }
   };
 
